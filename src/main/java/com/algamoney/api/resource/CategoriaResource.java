4d78b72e8b11cc032d0e1b62fc8c7076575d0cf1
wemmelotec <wellington.magno@gmail.com>
2021-02-22 15:14:55 -0300

    implementei o create

--- src/main/java/com/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/algamoney/api/resource/CategoriaResource.java
@@ -1,14 +1,26 @@
 package com.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.Servlet;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algamoney.api.model.Categoria;
 import com.algamoney.api.repository.CategoriaRepository;
+import com.sun.el.stream.Optional;
 
 //Essa é a classe que vai expor tudo que estiver relacionado ao recurso Categoria
 
@@ -20,8 +32,38 @@ public class CategoriaResource {
 	private CategoriaRepository categoriaRepository;
 	
 	@GetMapping//mapear para receber requisições do tipo get em categorias
+	//segunda versão do get que trata o recebimento de uma lista vazia
+	public ResponseEntity<?> listar(){
+		List<Categoria> categorias = categoriaRepository.findAll();
+		return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.noContent().build();//o build é necessário para gerar um response entity
+	}
+	/*
+	 * primeira versão do get 
 	public List<Categoria> listar(){
 		return categoriaRepository.findAll();
 	}
-
+	*/
+	@PostMapping
+	//@ResponseStatus(HttpStatus.CREATED)
+	//segunda versão do post, já implementando Rest, que indica que após salvar um objeto precisa retornar o location através do header
+	//e retorna também o recurso salvo
+	public ResponseEntity<Categoria> salvar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		 Categoria categoriaSalva =  categoriaRepository.save(categoria);
+		 
+		 URI uri =  ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoria.getCodigo()).toUri();
+		 response.setHeader("Location", uri.toASCIIString());
+		 
+		 return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	/*primeira versão do post
+	 * 
+	 public void salvar(@RequestBody Categoria categoria) {
+	 categoriaRepository.save(categoria);
+	}
+	*/
+	
+	@GetMapping("/{codigo}")
+	public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
+		  return this.categoriaRepository.findById(codigo).orElse(null);
+	}
 }
