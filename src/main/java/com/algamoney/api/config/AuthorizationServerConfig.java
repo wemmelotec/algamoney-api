ee52e46b704c56571ce3df1df88c107f1fbd6abb
wemmelotec <tonmelo@hotmail.com>
2022-05-13 09:40:22 -0300

    end of class 6.3

---
+++ src/main/java/com/algamoney/api/config/AuthorizationServerConfig.java
@@ -0,0 +1,59 @@
+package com.algamoney.api.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+
+	// configurar a aplicação (o cliente) que é diferente do usuário
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory().withClient("angular").secret("1111").scopes("read", "write")// não é a string que define, mas
+																						// sim o que fazemos com ela
+																						// depois
+				.authorizedGrantTypes("password").accessTokenValiditySeconds(1800);
+		// .refreshTokenValiditySeconds(3600 * 24);//tempo de vida do refresh token um
+		// dia
+	}
+
+	// continuação da configuração
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.tokenStore(tokenStore())
+				// .accessTokenConverter(accessTokenConverter())
+				// .reuseRefreshTokens(false)
+				.authenticationManager(authenticationManager);
+	}
+	/*
+	 * criei esse método e anotei como Bean para poder injetar ele em qualquer lugar
+	 * que eu precisar na aplicacao por isso eu criei esse metodo que retorna o tipo
+	 * de instacia que eu quero
+	 */
+	// @Bean//quem precisar de um access token converte basta chamar ele
+	// public JwtAccessTokenConverter accessTokenConverter() {
+	// JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+	// accessTokenConverter.setSigningKey("algaworks");
+	// return accessTokenConverter;
+	// }
+
+	// criado para armazenar o token
+	@Bean
+	public TokenStore tokenStore() {
+		return new InMemoryTokenStore();
+	}
+
+}
---
+++ src/main/java/com/algamoney/api/config/ResourceServerConfig.java
@@ -0,0 +1,47 @@
+package com.algamoney.api.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+@Configuration // indica que essa é uma classe de configuração
+@EnableWebSecurity // habilita essa classe para segurança
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+	@Autowired // para injetar o AuthenticationManagerBuilder
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// buscar autenticação em memória
+		auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ROLE");
+	}
+	
+
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		// para categorias qualquer um pode acessar para o resto eu tenho que estar
+		// autenticado
+		http.authorizeRequests().antMatchers("/categorias").permitAll().anyRequest().authenticated().and()// e
+				// .httpBasic().and()//tipo de autenticação http
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()// nossa API não mantém
+																									// estado, não cria
+																									// sessões
+				.csrf().disable();// como eu não uso JS na aplicação eu posso desabilitar
+
+	}
+	
+	//para deixar STATELESS, ou seja, a parte de segurança eu não quero que deixe nenhum estado, ou seja, não armazena nada
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.stateless(true);
+    }
+
+}
