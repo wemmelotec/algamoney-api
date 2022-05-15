310aacb42a45a453f4fcc66580f3454f037bc088
wemmelotec <tonmelo@hotmail.com>
2022-05-15 17:08:30 -0300

    end of class 6.10 cors

--- src/main/java/com/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/algamoney/api/resource/CategoriaResource.java
@@ -1,18 +1,15 @@
 package com.algamoney.api.resource;
 
-import java.net.URI;
 import java.util.List;
 
-import javax.servlet.Servlet;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,12 +18,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algamoney.api.event.RecursoCriadoEvent;
 import com.algamoney.api.model.Categoria;
 import com.algamoney.api.repository.CategoriaRepository;
-import com.sun.el.stream.Optional;
 
 
 //Essa é a classe que vai expor tudo que estiver relacionado ao recurso Categoria
@@ -41,6 +36,7 @@ public class CategoriaResource {
 	@Autowired//responsável por disparar o evento de recurso criado a partir desse objeto
 	private ApplicationEventPublisher publisher;
 	
+	//@CrossOrigin(maxAge = 10, origins = {"http://localhost:8000"})
 	@GetMapping//mapear para receber requisições do tipo get em categorias
 	public ResponseEntity<?> listar(){
 		List<Categoria> categorias = categoriaRepository.findAll();
