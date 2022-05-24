1714682ae262a64e125d4e096ca3be98b5fc59f4
wemmelotec <tonmelo@hotmail.com>
2022-05-24 13:28:06 -0300

    Revert "end of class 7.5"
    This reverts commit b6f4dd7793143dac242daf506604d7133b1f7e22.

--- src/main/java/com/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/algamoney/api/config/AuthorizationServerConfig.java
@@ -1,26 +1,19 @@
 package com.algamoney.api.config;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenEnhancer;
-import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
-import com.algamoney.api.config.token.CustomTokenEnhancer;
-
-@Profile("oauth-security")
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
@@ -52,18 +45,15 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		
 	}
 
-	//método para customizar o token
+	// continuação da configuração
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		
-		TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
-		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter()));
-		
 		endpoints
-				.tokenStore(tokenStore())
-				.tokenEnhancer(tokenEnhancerChain)
-				.reuseRefreshTokens(false)
-				.authenticationManager(authenticationManager);
+			.tokenStore(tokenStore())
+	        .accessTokenConverter(this.accessTokenConverter())
+	        .reuseRefreshTokens(false)
+	        .userDetailsService(this.userDetailsService)
+	        .authenticationManager(this.authenticationManager);
 	}
 	/*
 	 * criei esse método e anotei como Bean para poder injetar ele em qualquer lugar
@@ -82,9 +72,5 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	public TokenStore tokenStore() {
 		return new JwtTokenStore(accessTokenConverter());
 	}
-	
-	@Bean
-	public TokenEnhancer tokenEnhancer() {
-	    return new CustomTokenEnhancer();
-	}
+
 }
